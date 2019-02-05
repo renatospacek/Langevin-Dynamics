79b23e14b80553b031ebd51afdeb21e3042ce5ed
Renato Spacek <47311498+renatospacek@users.noreply.github.com>
2019-02-05 10:49:20 -0500

    Update input.jl

--- input.jl
+++ input.jl
@@ -6,18 +6,18 @@
 
 # nPart must be must be n^dim, where n ∊ ℕ
 
-t 	        = 1.0                                                               # Total simulation time
-N 	        = 2^10                                                              # Number of timesteps
+t 	    = 1.0                                                               # Total simulation time
+N 	    = 2^10                                                              # Number of timesteps
 dt          = t/N                                                               # Timestep size
-dim	        = 3                                                                 # Dimensionality of the system
+dim	    = 3                                                                 # Dimensionality of the system
 nPart       = 8                                                                 # Number of particles
-L	        = 1.0                                                               # Side length of simulation box
+L	    = 1.0                                                               # Side length of simulation box
 ρ           = nPart/L^dim                                                       # Density of particles in simulation box
-γ 	        = 1.0                                                               # Damping coefficient
-β 	        = 1.0                                                               # Inverse temperature
-σ	        = 1.0                                                               # LJ parameter | V(σ) = 0
-ε	        = 1.0                                                               # LJ parameter | depth of potential well
-rc	        = 2^(1/6)*σ                                                         # Interaction cutoff distance      
+γ 	    = 1.0                                                               # Damping coefficient
+β 	    = 1.0                                                               # Inverse temperature
+σ	    = 1.0                                                               # LJ parameter | V(σ) = 0
+ε	    = 1.0                                                               # LJ parameter | depth of potential well
+rc	    = 2^(1/6)*σ                                                         # Interaction cutoff distance      
 R           = 1                                                                 # Timestep size multiplier
 p           = 3                                                                 # Number of timestep sizes used
 M           = 1000                                                              # Number of dW vectors averaged over
