c8b3c200317029b95550dbea4a1a3aa381b57cc5
Renato Spacek <47311498+renatospacek@users.noreply.github.com>
2019-02-21 17:05:48 -0500

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,5 +1,7 @@
 #!/usr/local/bin/julia
+
 using InteractiveUtils
+
 include("input.jl")
 include("initialize.jl")
 include("integrator.jl")
@@ -8,22 +10,12 @@ include("histogram.jl")
 # ==============================================================================
 #                           Execute simulation
 # ==============================================================================
-
 function main()
-#    println("________________________________")
-#    println("dt = $(dt) | $(nPart) particles | Number of cells = $(Ncel)")
-#    println("________________________________")
-    println("Initializing positions and velocities...")
     X = initialize_box()
-    
-    println("Computing initial forces...")
     X = compute_force(X)
-    
     X = kinetic(X)
     
     dW = sqrt(dt)*randn(dim, nPart*N)
-    
-    println("Starting integration...")
     Nsteps = trunc(Int, N/R)
 
     for i = 1:Nsteps
@@ -35,9 +27,7 @@ function main()
         end
     end
     
-    println("Plotting histogram...")
     histogram(X.sp)
-    
 end
 
 
@@ -48,7 +38,6 @@ function vscale(p::Array{Float64})
     
     return p
 end
-
 
 @time main()
 
