99d7f906c4141a9eeee9d8252fa8f21757e02f8c
Renato Spacek <47311498+renatospacek@users.noreply.github.com>
2019-02-21 17:08:35 -0500

    Update PBC.jl

--- PBC.jl
+++ PBC.jl
@@ -3,7 +3,7 @@
 include("input.jl")
 
 # ==============================================================================
-function PBC(q::Array{Float64,2}) # position between -L/2 and L/2
+function PBC(q::Array{Float64,2})
     for i in 1:nPart
         for j in 1:dim
             q[j,i] = mod(q[j,i]+L/2, L) - L/2   
@@ -14,7 +14,7 @@ function PBC(q::Array{Float64,2}) # position between -L/2 and L/2
 end
 
 # ==============================================================================
-function PBC(q::Array{Float64,1}) # distance between particles
+function PBC(q::Array{Float64,1})
     for j in 1:dim
         q[j] = mod(q[j]+L/2, L) - L/2   
     end
