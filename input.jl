14007a358715f6b52b100e8454ed719660ec4d24
Renato Spacek <47311498+renatospacek@users.noreply.github.com>
2019-02-21 17:07:09 -0500

    Update input.jl

--- input.jl
+++ input.jl
@@ -9,23 +9,19 @@ using LaTeXStrings
 # ==============================================================================
 #                           Simulation parameters
 # ==============================================================================
-
-const t 	        = 1.0        # Total simulation time                          
-const N 	        = 10^4        # Number of timesteps
+const t 	    = 1.0        # Total simulation time                          
+const N 	    = 10^4        # Number of timesteps
 const dt            = t/N         # Timestep                                 
-const dim	        = 3           # Dimensionality of the system                                  
+const dim	    = 3           # Dimensionality of the system                                  
 const ndim          = 10          # Number of particles per side                                 
 const nPart         = ndim^dim    # Number of particles 
 const L	            = 18          # Side length of simulation box                                                      
-const γ 	        = 1           # Damping                                 
-const β 	        = 1           # Inverse temperature                                 
+const γ 	    = 1           # Damping                                 
+const β	            = 1           # Inverse temperature                                 
 const σ	            = 1           # Lennard-Jones, V(σ) = 0                                
 const ε	            = 1           # Lennard-Jones, depth of potential well                                
-const rc	        = 2^(1/6)*σ   # Cutoff distance                          
-const R             = 1           # Timestep scalar                         
-#const p             = 3                                    
-#const avgM          = 10     
-#const sFreq         = 1
+const rc	    = 2^(1/6)*σ   # Cutoff distance                          
+const R             = 1           # Timestep scalar       
 const outFreq       = 100         # Output every outFreq timesteps
 
 # ======= Cell list parameters =======
@@ -36,7 +32,6 @@ const Ncel = M^3                  # Number of cells
 # ==============================================================================
 #                         Base/Type definitions
 # ==============================================================================
-
 Base.show(io::IO, f::Float64) = @printf(io, "%1.3f", f)
 
 mutable struct particle{T<:AbstractFloat}
