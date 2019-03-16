d872bec50bee6b3dcaee4898a59a36169d7d376b
Renato Spacek <47311498+renatospacek@users.noreply.github.com>
2019-03-16 08:25:45 -0400

    Update cell_lists.jl

--- cell_lists.jl
+++ cell_lists.jl
@@ -5,73 +5,94 @@ include("hamiltonian.jl")
 include("PBC.jl")
 
 # ==============================================================================
-#	                         Initialize list
-# ==============================================================================
-function initialize_list(X::particle)
-    dx = zeros(Float64, 3)
-    mc = zeros(Int, 3)
-    head = zeros(Int, Ncel)
-    list = zeros(Int, nPart)
-    
+function build_list!(X::particle{Float64}, Z::clist)
+   
     for i in 1:nPart
-        for j in 1:dim
-            dx[j] = mod(X.q[j,i] + L/2, L)
-            mc[j] = floor(Int, dx[j]/L*M)   
-        end
-        c =  1 + mc[1] + mc[2]*M + mc[3]*M^2
-
-        list[i] = head[c]
-        head[c] = i
+        vec_index!(X, Z, i)  
+        c = 1 + Z.mc[1] + Z.mc[2]*M + Z.mc[3]*M^2
+        
+        @inbounds Z.list[i] = Z.head[c]
+        @inbounds Z.head[c] = i
     end
-    
-    return head, list
+end
+
+# ==============================================================================
+function clear_list!(Z::clist)
+
+    fill!(Z.list, 0)
+    fill!(Z.head, 0)
+
 end
 
 # ==============================================================================
 #                           Compute interactions
 # ==============================================================================
-function compute_force(X::particle)
-    pe = 0.0
-    X.f = zeros(dim, nPart)
+function compute_force!(X::particle{Float64}, Z::clist)
+    
+    fill!(X.f, 0.0)
     X.pe = 0.0
-    head, list = initialize_list(X)
-    mc = zeros(Int, 3)
-    dx = zeros(3)
-    ci = zeros(Int, 3)
+    
+    if nopot == 1
+        return X
+    end
+    
+    clear_list!(Z)
+    build_list!(X, Z)
 
     for i in 1:nPart       
-        for j in 1:dim
-            dx[j] = mod(X.q[j,i] + L/2, L)
-            mc[j] = floor(Int, dx[j]/L*M)
-        end
+        vec_index!(X, Z, i)
         
-        for ci[1] in (mc[1]-1):(mc[1]+1),
-            ci[2] in (mc[2]-1):(mc[2]+1),
-            ci[3] in (mc[3]-1):(mc[3]+1)
+        for ci1 in (Z.mc[1]-1):(Z.mc[1]+1),
+            ci2 in (Z.mc[2]-1):(Z.mc[2]+1),
+            ci3 in (Z.mc[3]-1):(Z.mc[3]+1)
 
-            for j in 1:dim
-                ci[j] = mod(ci[j], M)
-            end
-            
-            c = 1 + ci[1] + ci[2]*M + ci[3]*M^2
-            j = head[c]
+            adjc = 1 + cind(ci1) + cind(ci2)*M + cind(ci3)*M*M
+            j = Z.head[adjc]
 
             while j != 0
                 if i<j
-                    dr = X.q[:,i] - X.q[:,j]
-                    r = PBC(dr)   
-                    ff, pe = LJ(r) 
-                    
-                    X.f[:,i] += ff*r
-                    X.f[:,j] -= ff*r
-                    X.pe += pe
+                    update_force!(X, Z, i, j)
                 end
-                j = list[j]
+                @inbounds j = Z.list[j]
             end
         end
     end
-
-    return X
 end
 
+# ==============================================================================
+function update_force!(X::particle{Float64}, 
+                       Z::clist,
+                       i::Int64,
+                       j::Int64)
+
+    for l in 1:3
+        Z.r[l] = X.q[l,i] - X.q[l,j]
+    end
+    
+    dist = PBC(Z)   
+    ff = fLJ(dist)
+    X.pe += peLJ(dist)
+
+    for l in 1:3    
+        X.f[l,i] += ff*Z.r[l]
+        X.f[l,j] -= ff*Z.r[l]
+    end  
+end
+
+# ==============================================================================
+function vec_index!(X::particle{Float64}, Z::clist, i::Int)
+    for j in 1:dim
+        @inbounds Z.dx[j] = remap(X.q[j,i] + L2)
+        @inbounds Z.mc[j] = floor(Int, Z.dx[j]/L*M)   
+    end
+end
+
+# ==============================================================================
+function cind(x::Int64)::Int64
+
+    while x >= M; x -= M; end
+    while x < 0; x += M; end
+    
+    return x
+end
 
